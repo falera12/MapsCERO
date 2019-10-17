457589c7598a291e94050ca101d1466d83bf56c8
falera12 <56373683+falera12@users.noreply.github.com>
2019-10-16 21:47:46 -0500

    MapsFragments
    Fragments-no completo

--- app/src/main/java/com/example/mapscero/MainActivity.java
+++ app/src/main/java/com/example/mapscero/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.mapscero;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.mapscero.Fragments.Welcome;
 
+public class MainActivity extends AppCompatActivity {
+    Fragment currentFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currentFragment = new Welcome();
+
+
+
     }
+
+    private void changeFragment(Fragment fragment){
+    get
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
