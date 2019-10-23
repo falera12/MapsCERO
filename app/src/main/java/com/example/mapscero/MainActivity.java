d42d90e660332f1bf6129b996dbcded206eed60e
falera12 <56373683+falera12@users.noreply.github.com>
2019-10-22 22:29:32 -0500

    Fragment-Con Maps o algo asi
    Fragment,maps,current fragment, cambiar fragment inicial

--- app/src/main/java/com/example/mapscero/MainActivity.java
+++ app/src/main/java/com/example/mapscero/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.mapscero;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.example.mapscero.Fragments.Map;
 import com.example.mapscero.Fragments.Welcome;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,13 +19,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         currentFragment = new Welcome();
-
-
+        changeFragment(currentFragment);
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_welcome:
+                currentFragment = new Welcome();
+                break;
+
+            case R.id.menu_map:
+                currentFragment = new Map();
+                break;
+        }
+        changeFragment(currentFragment);
+        return super.onOptionsItemSelected(item);
+    }
+
     private void changeFragment(Fragment fragment){
-    get
+    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
 
 
     }
